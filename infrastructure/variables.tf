023a8310c9cc8e8a8c6b5f05c609c4912efdce7d
justin <stric@duck.com>
2026-06-06 10:16:14 -0400

    Fix infrastructure names: update to original oke-infrastructure naming for state bucket and cluster

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -11,7 +11,7 @@ variable "tenancy_ocid" {
 variable "cluster_name" {
   description = "Name of the OKE cluster."
   type        = string
-  default     = "hineat"
+  default     = "oke-infrastructure"
 }
 
 variable "kubernetes_version" {
